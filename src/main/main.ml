15a16b285e0f04de465969b3ec9e3adcfc647913
Ilya Sergey <i.sergey@ucl.ac.uk>
2018-02-13 19:42:02 +0000

    cleanup

--- src/main/main.ml
+++ src/main/main.ml
@@ -1,10 +1,9 @@
 open Printf
-open Pervasives
 
 let () =
   let foo z =
     for i = 1 to Array.length Sys.argv - 1 do
-      printf "[%i] %d\n" i (int_of_string (Sys.argv.(i)))
+      printf "[%i] %d\n" i (Sys.argv.(i) |> int_of_string)
     done 
   in foo 5
 
