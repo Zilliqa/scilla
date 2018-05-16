589ee56ec15e018cc6aca542a6c989f3582349b4
Vaivaswatha Nagaraj <vaivaswatha@zilliqa.com>
2018-05-16 16:31:01 +0800

    Testsuite (OCaml) add comparison tests for output json.

--- examples/Testsuite.ml
+++ examples/Testsuite.ml
@@ -5,6 +5,9 @@ let main =
   let examples_dir_default = (Sys.getcwd () ^ Filename.dir_sep ^ "examples") in
   let bin_dir = Conf.make_string "bin_dir" bin_dir_default "directory containing binaries" in
   let examples_dir = Conf.make_string "examples_dir" examples_dir_default "directory containing examples" in
+
   (* Add calls to new tests from here *)
   let contract_tests = Testcontracts.add_tests bin_dir examples_dir in
-    run_test_tt_main contract_tests
+
+  (* Run all tests *)
+  run_test_tt_main contract_tests
--- examples/contracts/Testcontracts.ml
+++ examples/contracts/Testcontracts.ml
@@ -3,6 +3,15 @@ open OUnit2
 let i_to_s i =
   Printf.sprintf "%d" i
 
+(* load an entire file to memory *)
+let load_file f =
+  let ic = open_in f in
+  let n = in_channel_length ic in
+  let s = Bytes.create n in
+  really_input ic s 0 n;
+  close_in ic;
+  (s)
+  
 (* 
  * Build tests to invoke scilla-runner with the right arguments, for
  * multiple test cases, each suffixed with _i up to _n (both inclusive)
@@ -11,19 +20,26 @@ let rec build_contract_tests bindir examplesdir name i n =
   if (i > n) 
     then [] 
   else
-    let test = "crowdfunding_"^(Printf.sprintf "%d"  i) >::
+    let test = name ^"_"^(i_to_s  i) >::
+    (* function to run scilla-runner and check exit code *)
       (fun test_ctxt ->
         let dir = examplesdir test_ctxt ^ Filename.dir_sep ^ "contracts" ^ Filename.dir_sep ^
           name ^ Filename.dir_sep in
+        let tmpdir = bracket_tmpdir test_ctxt in 
+        let output_file = tmpdir ^ Filename.dir_sep ^ name ^ "_output_"
+                    ^ (i_to_s  i) ^ ".json" in
         let args = ["-init"; dir ^ "init.json"; 
               "-i"; dir ^ "contract";
-              "-o"; (bracket_tmpdir test_ctxt) ^ Filename.dir_sep ^ "output_"
-                    ^ (i_to_s  i) ^ ".json";
+              "-o"; output_file;
               "-imessage"; dir ^ "message_" ^ (i_to_s i) ^ ".json";
               "-istate" ; dir ^ "state_" ^ (i_to_s i) ^ ".json";
               "-iblockchain" ; dir ^ "blockchain_" ^ (i_to_s i) ^ ".json"] in
         let scillabin = bindir test_ctxt ^ Filename.dir_sep ^ "scilla-runner" in
-          assert_command test_ctxt scillabin args
+          (assert_command test_ctxt scillabin args;
+           let goldoutput_file = dir ^ "output_" ^ (i_to_s i) ^ ".json" in
+           let g = load_file goldoutput_file in
+           let o = load_file output_file in
+           assert_equal ~ctxt:test_ctxt ~msg:"Output json mismatch" g o);
       ) 
       in
       test :: (build_contract_tests bindir examplesdir name (i+1) n)
