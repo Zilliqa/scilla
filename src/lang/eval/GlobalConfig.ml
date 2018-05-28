0fae68169e0ab34d0a37c5e41f95ada579308156
Vaivaswatha N <vaivaswatha@users.noreply.github.com>
2018-05-29 07:02:07 +0800

    Debug Utilities (#52)
    * Debug message utility. #14
    * Debug message utility: add automatic file creation based on log number
    * Idiomatic handling of output channels
    * Renamed debug files

---
+++ src/lang/eval/GlobalConfig.ml
@@ -0,0 +1,61 @@
+(*
+ * Copyright (c) 2018 - present Zilliqa, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *)
+
+(* Available debug levels for functions in DbgMsg *)
+type debug_kind =
+  | Debug_None
+  | Debug_Normal
+  | Debug_Verbose
+
+open Filename
+
+let debug_level = ref Debug_None
+let log_file = ref ""
+
+let rec get_highest_numbered_log files =
+  match files with
+  | [] ->
+    0
+  | file :: files' ->
+    let found = Str.string_match (Str.regexp "scilla-runner-\\([0-9]+\\)\\.log") file 0 in
+    let substr = Str.matched_string file in
+    if found
+    then
+      let numstr = Str.matched_group 1 substr in
+      let num = Core.int_of_string numstr in
+      let num' = get_highest_numbered_log files' in
+        Core.Int.max num num'
+    else
+      0
+
+(* Given a directory, look for consecutively named files
+ * scilla-runner-[0-9]+.log and return the next in sequence *)
+let create_log_filename dir =
+  if not (Sys.file_exists dir) || 
+     not (Sys.is_directory dir)
+  then
+    Unix.mkdir dir 0o766; (* Arbitrary *)
+  let files = Sys.readdir dir in
+  let num = get_highest_numbered_log (Array.to_list files) in
+  dir ^ dir_sep ^ "scilla-runner-" ^ Core.Int.to_string (num+1) ^ ".log"
+
+let get_debug_level () =
+  !debug_level
+
+let set_debug_level l =
+  debug_level := l
+
+let get_log_file () =
+  if !log_file = ""
+  then
+    log_file := create_log_filename ("_build" ^ dir_sep ^ "logs");
+  !log_file
+
+let set_log_file s =
+  log_file := s
