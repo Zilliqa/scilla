86da0d370854b3fd7ea858e9ad041c1d8502ecbe
Ilya Sergey <ilyaserge@fb.com>
2018-05-28 12:57:51 +0100

    Added recursoin for natural numbers

--- src/lang/eval/EvalUtil.ml
+++ src/lang/eval/EvalUtil.ml
@@ -42,6 +42,7 @@ module Env = struct
   'rep value =
     | ValLit of literal
     | ValClosure of 'rep Syntax.ident * typ * 'rep expr * 'rep t
+    | ValFix of 'rep Syntax.ident * 'rep Syntax.ident * typ * 'rep expr * 'rep t
   [@@deriving sexp]
 
   (* Pretty-printing *)
@@ -53,6 +54,7 @@ module Env = struct
   and
     pp_value v = match v with
     | ValLit l -> sexp_of_literal l |> Sexplib.Sexp.to_string
+    | ValFix _ -> "<fixpoint>"
     | ValClosure (f, t, e, env) -> "<closure>"
         (* (sexp_of_expr sexp_of_loc (Fun (f, t, e)) |> Sexplib.Sexp.to_string)
          * ^ ", " ^ (pp env)   *)
@@ -61,6 +63,9 @@ module Env = struct
 
   let bind e k v =
     (k, v) :: List.filter ~f:(fun z -> fst z <> k) e
+
+  let bind_all e kvs = 
+    List.fold_left ~init:e ~f:(fun z (k, v) -> bind z k v) kvs
                                                                 
   let lookup e k =
     let i = get_id k in
@@ -125,7 +130,7 @@ module Configuration = struct
   
   let store st k v =
     match v with 
-    | Env.ValClosure _ ->
+    | Env.ValClosure _ | Env.ValFix _ ->
         fail @@ sprintf "Cannot store a closure below into a field %s:\n%s"
           k (Env.pp_value v)
     | Env.ValLit l ->
@@ -197,7 +202,7 @@ module Configuration = struct
         | l -> fail @@ sprintf "The literal is not a list:\n%s" (pp_literal l))
       in       
       match v with
-      | Env.ValClosure _ as v ->
+      | (Env.ValFix _ | Env.ValClosure _) as v ->
           fail @@
           sprintf "Value should be a list of messages, but is a closure:\n%s"
             (Env.pp_value v)
--- src/runners/eval_runner.ml
+++ src/runners/eval_runner.ml
@@ -12,15 +12,17 @@ open Printf
 open Sexplib.Std
 open Syntax
 open EvalUtil
+open Recursion
 
 let () =
   let filename = Sys.argv.(1) in
   match FrontEndParser.parse_file ScillaParser.exps filename with
   | Some (e :: _) ->
-      let env = Env.empty in
+      let env = Env.bind_all Env.empty recursion_principles in
       let res = Eval.exp_eval e env in
       (match res with
-      | Ok r -> printf "%s\n" (Eval.pp_result res)
+      | Ok (v, env) -> 
+          printf "%s\n" (Eval.pp_result res)
       | Error _ -> printf "Failed execution:\n%s\n" (Eval.pp_result res))
   | Some [] | None ->
       printf "%s\n" "Failed to parse input file."
