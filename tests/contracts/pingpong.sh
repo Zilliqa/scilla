2f733a0990419e0f8946542e269e01a910dae8eb
Vaivaswatha Nagaraj <vaivaswatha@zilliqa.com>
2018-06-01 12:23:59 +0800

    PingPong: add SetPingAddr/SetPongAddr to pingpong.sh for visualization

--- tests/contracts/pingpong.sh
+++ tests/contracts/pingpong.sh
@@ -2,6 +2,18 @@
 
 cd ../../
 
+echo "SetPongAddr"
+bin/scilla-runner -init tests/contracts/ping/init.json -i tests/contracts/ping/contract.scilla -o /tmp/ping_output.json -imessage tests/contracts/ping/message_0.json -istate tests/contracts/ping/state_0.json -iblockchain tests/contracts/ping/blockchain_0.json
+echo "Output state of Ping after setting pong address"
+cat /tmp/ping_output.json
+echo ""
+
+echo "SetPingAddr"
+bin/scilla-runner -init tests/contracts/pong/init.json -i tests/contracts/pong/contract.scilla -o /tmp/pong_output.json -imessage tests/contracts/pong/message_0.json -istate tests/contracts/pong/state_0.json -iblockchain tests/contracts/pong/blockchain_0.json
+echo "Output state of Pong after setting ping address"
+cat /tmp/pong_output.json
+echo ""
+
 echo "Ping ..."
 bin/scilla-runner -init tests/contracts/ping/init.json -i tests/contracts/ping/contract.scilla -o /tmp/ping_output.json -imessage tests/contracts/ping/message_1.json -istate tests/contracts/ping/state_1.json -iblockchain tests/contracts/ping/blockchain_1.json
 echo "Output state of Ping after pinging"
