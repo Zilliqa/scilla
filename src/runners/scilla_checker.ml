5bdaa5a9dbf06cbf84e25c63c873f05073139aea
Vaivaswatha N <vaivaswatha@live.in>
2018-06-11 21:49:56 +0530

    Create a separate binary for just parsing scilla code, to be used by JavaScript editor etc to report potential syntax errors. #70

---
+++ src/runners/scilla_checker.ml
@@ -0,0 +1,23 @@
+(*
+ * Copyright (c) 2018 - present Zilliqa, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *)
+
+
+open Printf
+open Sexplib.Std
+open Syntax
+open Core
+open DebugMessage
+
+let () =
+  let parse_module =
+    FrontEndParser.parse_file ScillaParser.cmodule Sys.argv.(1) in
+  match parse_module with
+  | None -> plog (sprintf "%s\n" "Failed to parse input file.")
+  | Some cmod ->
+      plog (sprintf "\n[Parsing]:\nContract module [%s] is successfully parsed.\n" Sys.argv.(1));
