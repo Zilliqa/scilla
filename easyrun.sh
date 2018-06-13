8c8098caade46fa3f61ed1ce4262558dd9aa0f94
Vaivaswatha Nagaraj <vaivaswatha@zilliqa.com>
2018-06-13 12:34:49 +0530

    add file missing in previous commit

--- easyrun.sh
+++ easyrun.sh
@@ -1,23 +1,40 @@
 #!/bin/bash
 
-function print_usage
-if [ $# != 3 ]
+function print_available_contracts
+{
+    for ff in tests/contracts/*
+    do
+        if [[ -d $ff && $ff != "tests/contracts/other" ]]
+        then
+            f=`basename $ff`
+            echo -n "$f|"
+        fi
+    done
+}
+
+function print_usage_and_exit
+{
+    echo -n "Usage: $0 ["
+    print_available_contracts
+    echo "] test_number"
+    exit 1
+}
+
+
+if [ $# != 2 ]
 then
-    echo "Usage: $0 contract-name test_number"
+   print_usage_and_exit
 fi
 
 contract=$1
 i=$2
 
-
 cdir="tests/contracts/$contract"
 
-if [[ ! -f ${cdir}/state_${i}.json ||
-      ! -d ${cdir} ]]
+if [[ ! -d ${cdir} || ! -f ${cdir}/state_${i}.json ]]
 then
     echo "Test $contract $i does not exist"
-    print_usage
-    exit 1
+    print_usage_and_exit
 fi
 
 ./bin/scilla-runner -init ${cdir}/init.json -istate ${cdir}/state_${i}.json -imessage ${cdir}/message_${i}.json -o ${cdir}/output_${i}.json -iblockchain ${cdir}/blockchain_${i}.json -i ${cdir}/contract.scilla
