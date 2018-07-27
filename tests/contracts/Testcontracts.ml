04c4ecaca6e8b81573931365242500f41842119a
Edison <edison.limjh@gmail.com>
2018-07-27 18:24:40 +0800

    Added bookstore test contract and updated test cases

--- tests/contracts/Testcontracts.ml
+++ tests/contracts/Testcontracts.ml
@@ -139,8 +139,9 @@ let add_tests env =
     let helloWorldtests_f = "helloWorld_f" >:::(build_contract_tests env "helloWorld" fail_code 4 8) in
     let auctiontests = "auction" >:::(build_contract_tests env "auction" succ_code 1 8) in
     let mappairtests = "mappair" >:::(build_contract_tests env "mappair" succ_code 1 5) in
+    let bookstoretests = "bookstore" >:::(build_contract_tests env "bookstore" succ_code 1 10) in
 
     let fungibletokentests = "fungible-token" >:::(build_contract_tests env "fungible-token" succ_code 0 8) in
     let misc_tests = "misc_tests" >::: build_misc_tests env in
       "contract_tests" >::: [crowdfundingtests;cfinit_test;zilgametests;zginit_test;cfinvoketests;mappairtests;
-                             misc_tests;pingtests;pongtests;fungibletokentests;helloWorldtests;helloWorldtests_f;auctiontests]
+                             misc_tests;pingtests;pongtests;fungibletokentests;helloWorldtests;helloWorldtests_f;auctiontests;bookstoretests]
