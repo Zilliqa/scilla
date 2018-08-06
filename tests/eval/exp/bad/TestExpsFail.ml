69afa04665fd8250861811ba7ee6fd6010c2ab7c
jjcnn <jajocnn@gmail.com>
2018-08-06 14:33:33 +0200

    More test fixes

--- tests/eval/exp/bad/TestExpsFail.ml
+++ tests/eval/exp/bad/TestExpsFail.ml
@@ -19,14 +19,26 @@
 open OUnit2
 
 let explist = [
-  "app_error1.scilla"; "app_error2.scilla"; "builtin4.scilla";
-  "builtin_error1.scilla"; "builtin-overflow1.scilla";
-  "builtin-overflow2.scilla"; "builtin-overflow3.scilla";
-  "builtin-overflow4.scilla"; "builtin-overflow5.scilla";
-  "builtin-overflow6.scilla"; "builtin-overflow7.scilla";
-  "builtin-overflow8.scilla"; "builtin-overflow9.scilla";
-  "builtin-overflow10.scilla"; "builtin-overflow11.scilla";
-  "let-error.scilla"; "string_error1.scilla";
+  "app_error1.scilla";
+  "app_error2.scilla";
+  "builtin4.scilla";
+  "builtin_error1.scilla";
+  "builtin-overflow10.scilla";
+  "builtin-overflow11.scilla";
+  "builtin-overflow1.scilla";
+  "builtin-overflow2.scilla";
+  "builtin-overflow3.scilla";
+  "builtin-overflow4.scilla";
+  "builtin-overflow5.scilla";
+  "builtin-overflow6.scilla";
+  "builtin-overflow7.scilla";
+  "builtin-overflow8.scilla";
+  "builtin-overflow9.scilla";
+  "msg_error2.scilla";
+  "msg_error3.scilla";
+  "msg_error.scilla";
+  "let-error.scilla";
+  "string_error1.scilla";
 ]
 
 module Tests = TestUtil.DiffBasedTests(
