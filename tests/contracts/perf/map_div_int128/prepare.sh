33f6798ba39cddb49bbfd497fe84a92e44b79132
Vaivaswatha Nagaraj <vaivaswatha@zilliqa.com>
2018-10-03 18:13:10 +0530

    add mul and div experiments

---
+++ tests/contracts/perf/map_div_int128/prepare.sh
@@ -0,0 +1,16 @@
+
+update_state()
+{
+    states=`sed '1,4d' output.json | head -n -3`
+    echo -e "[\n$states \n]\n" > state.json
+}
+
+i=1
+while [[ $i -le 5000 ]]
+do
+    ../../../../bin/scilla-runner -init init.json -istate state.json -imessage message.json -o output.json -iblockchain blockchain.json -i contract.scilla -libdir ../../../..//src/stdlib -gaslimit 1000000
+    
+    update_state
+
+    i=$(($i + 1))
+done
---
+++ tests/contracts/perf/map_mul_int128/experiment.sh
@@ -0,0 +1,24 @@
+runs=1
+
+function runscilla
+{
+    n=$1
+    i=1
+    while [[ $i -le $runs ]]
+    do
+        ../../../../bin/scilla-runner -init init.json -istate state_${n}.json -imessage message.json -o output.json -iblockchain blockchain.json -i contract.scilla -libdir ../../../..//src/stdlib -gaslimit 1000000
+        
+        i=$(($i + 1))
+    done
+}
+
+function runexp
+{
+    n=$1
+    t=`(time runscilla ${1}) 2>&1 | tee -a timer-log.txt | grep "real" | cut -d $'\t' -f 2`
+    echo "Time for $runs runs of list size $1: $t"
+}
+
+rm timer-log.txt
+runexp 5000
+runexp 10000
