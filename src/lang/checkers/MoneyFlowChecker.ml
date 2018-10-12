a61fafd7bd46f64805106a9b07a384b910e57955
Jacob Johannsen <jajocnn@gmail.com>
2018-10-12 10:19:23 +0200

    Initial traversal of MoneyFlowChecker

--- src/lang/checkers/MoneyFlowChecker.ml
+++ src/lang/checkers/MoneyFlowChecker.ml
@@ -14,22 +14,22 @@
  
   You should have received a copy of the GNU General Public License along with
 *)
-(*
+
 open Syntax
 open TypeUtil
 
-
 module MoneyFlowRep (R : Rep) = struct
-  type MoneyTag =
+  type money_tag =
     | Plain
     | Money
-    | Map of MoneyTag
+    | Map of money_tag
     | Top
+  [@@deriving sexp]
     (* TODO: Add this if possible *)
   (*    | ADT of t *)
-  [@@deriving sexp]
       
-  type rep = MoneyTag * R.rep
+  type rep = money_tag * R.rep
+  [@@deriving sexp]
 
   let get_loc r = match r with | (_, rr) -> R.get_loc rr
 
@@ -44,8 +44,6 @@ module MoneyFlowRep (R : Rep) = struct
   
   let parse_rep s = (Plain, R.parse_rep s)
   let get_rep_str r = match r with | (_, rr) -> R.get_rep_str rr
-
-  let get_type (r : rep) = fst r
 end
 
 
@@ -57,130 +55,164 @@ module ScillaMoneyFlowChecker
      end) = struct
 
   module SMFR = SR
-  module EMFR = ER
+  module EMFR = MoneyFlowRep (ER)
   module TypedSyntax = ScillaSyntax (SR) (ER)
   module MFSyntax = ScillaSyntax (SMFR) (EMFR)
   (*   module TU = TypeUtilities (SR) (ER) *)
   (*   module SCU = ContractUtil.ScillaContractUtil (SR) (ER) *)
 
-  open MFSyntax
+  open TypedSyntax
   (*   open SCU *)
 
-  let rec mf_tag_expr (erep : TypedSyntax.expr_annot) =
-    let (e, rep) = erep in
-    match e with
-    | Literal l ->
-        let%bind lt = literal_type l in
-        pure @@ (TypedSyntax.Literal l, (mk_qual_tp lt, rep))
-    | Var i ->
-        let%bind r = TEnv.resolveT tenv (get_id i) ~lopt:(Some (get_rep i)) in
-        let typ = rr_typ r in
-        pure @@ (TypedSyntax.Var (add_type_to_ident i typ), (typ, rep))
-    |  Fun (arg, t, body) ->
-        let%bind _ = TEnv.is_wf_type tenv t in
-        let tenv' = TEnv.addT (TEnv.copy tenv) arg t in
-        let%bind (_, (bt, _)) as b = type_expr tenv' body in
-        let typed_arg = add_type_to_ident arg (mk_qual_tp t) in
-        pure @@ (TypedSyntax.Fun (typed_arg, t, b), (mk_qual_tp (FunType (t, bt.tp)), rep))
-    | App (f, actuals) ->
-        wrap_type_err erep @@ 
-        let%bind fres = TEnv.resolveT tenv (get_id f) ~lopt:(Some (get_rep f)) in
-        let%bind (typed_actuals, apptyp) = app_type tenv (rr_typ fres).tp actuals in
-        let typed_f = add_type_to_ident f (rr_typ fres) in
-        pure @@ (TypedSyntax.App (typed_f, typed_actuals), (apptyp, rep))
-    | Builtin (i, actuals) ->
-        wrap_type_err erep @@ 
-        let%bind (targs, typed_actuals) = type_actuals tenv actuals in
-        let%bind (_, ret_typ, _) = BuiltInDictionary.find_builtin_op i targs in
-        let%bind _ = TEnv.is_wf_type tenv ret_typ in
-        let q_ret_typ = mk_qual_tp ret_typ in
-        pure @@ (TypedSyntax.Builtin (add_type_to_ident i q_ret_typ, typed_actuals), (q_ret_typ, rep))
-    | Let (i, topt, lhs, rhs) ->
-        (* Poor man's error reporting *)
-        let%bind (_, (ityp, _)) as checked_lhs = wrap_type_err erep @@ type_expr tenv lhs in
-        let tenv' = TEnv.addT (TEnv.copy tenv) i ityp.tp in
-        let typed_i = add_type_to_ident i ityp in
-        let%bind (_, (rhstyp, _)) as checked_rhs = type_expr tenv' rhs in
-        pure @@ (TypedSyntax.Let (typed_i, topt, checked_lhs, checked_rhs), (rhstyp, rep))
-    | Constr (cname, ts, actuals) ->
-        let%bind _ = mapM ts ~f:(TEnv.is_wf_type tenv) in
-        let open Datatypes.DataTypeDictionary in 
-        let%bind (_, constr) = lookup_constructor cname in
-        let alen = List.length actuals in
-        if (constr.arity <> alen)
-        then fail0 @@ (sprintf
-            "Constructor %s expects %d arguments, but got %d."
-            cname constr.arity alen)
-        else
-          let%bind ftyp = elab_constr_type cname ts in
-          (* Now type-check as a function application *)
-          let%bind (typed_actuals, apptyp) = app_type tenv ftyp actuals in
-          pure @@ (TypedSyntax.Constr (cname, ts, typed_actuals), (apptyp, rep))
-    | MatchExpr (x, clauses) ->
-        if List.is_empty clauses
-        then fail0 @@ sprintf
-            "List of pattern matching clauses is empty:\n%s" (pp_expr e)
-        else
-          let%bind sctyp = TEnv.resolveT tenv (get_id x)
-              ~lopt:(Some (get_rep x)) in
-          let sct = (rr_typ sctyp).tp in
-          let msg = sprintf " of type %s" (pp_typ sct) in
-          wrap_type_err erep ~opt:msg (
-            let%bind typed_clauses = mapM clauses ~f:(fun (ptrn, ex) ->
-                type_check_match_branch tenv sct ptrn ex) in
-            let cl_types = List.map typed_clauses ~f:(fun (_, (_, (t, _))) -> t) in
-            let%bind _ =
-              assert_all_same_type (List.map ~f:(fun it -> it.tp) cl_types) in
-            (* Return the first type since all they are the same *)
-            pure @@ (TypedSyntax.MatchExpr
-                       (add_type_to_ident x (rr_typ sctyp),
-                        typed_clauses),
-                     (List.hd_exn cl_types, rep))
-          )
-    | Fixpoint (f, t, body) ->
-        wrap_type_err erep @@ 
-        let tenv' = TEnv.addT (TEnv.copy tenv) f t in
-        let%bind (_, (bt, _)) as typed_b = type_expr tenv' body in
-        let%bind _ = assert_type_equiv t bt.tp in
-        pure @@ (TypedSyntax.Fixpoint (add_type_to_ident f (mk_qual_tp t), t, typed_b), (mk_qual_tp t, rep))
-    | TFun (tvar, body) ->
-        let tenv' = TEnv.addV (TEnv.copy tenv) tvar in
-        let%bind (_, (bt, _)) as typed_b = type_expr tenv' body in
-        let typed_tvar = add_type_to_ident tvar bt in
-        pure @@ (TypedSyntax.TFun (typed_tvar, typed_b), (mk_qual_tp (PolyFun ((get_id tvar), bt.tp)), rep))
-    | TApp (tf, arg_types) ->
-        let%bind _ = mapM arg_types ~f:(TEnv.is_wf_type tenv) in
-        let%bind tfres = TEnv.resolveT tenv (get_id tf)
-            ~lopt:(Some (get_rep tf)) in
-        let tf_rr = rr_typ tfres in
-        let tftyp = tf_rr.tp in
-        let%bind res_type = elab_tfun_with_args tftyp arg_types in
-        let%bind _ = TEnv.is_wf_type tenv res_type in
-        pure @@ (TypedSyntax.TApp (add_type_to_ident tf tf_rr, arg_types), (mk_qual_tp res_type, rep))
-    | Message bs ->
-        let open PrimTypes in 
-        let payload_type pld =
-          (match pld with
-           | MTag m -> pure @@ TypedSyntax.MTag m
-           | MLit l ->
-               let%bind _ = type_expr tenv (Literal l, rep) 
-               in pure @@ TypedSyntax.MLit l
-           | MVar i ->
-               let%bind r = TEnv.resolveT tenv (get_id i)
-                   ~lopt:(Some (get_rep i)) in
-               let t = rr_typ r in
-               let rtp = t.tp in
-               if is_storable_type rtp
-               then pure @@ TypedSyntax.MVar (add_type_to_ident i t)
-               else fail1 (sprintf "Cannot send values of type %s." (pp_typ rtp))
-                          (ER.get_loc (get_rep i)))
-        in
-        let%bind typed_bs =
-          (* Make sure we resolve all the payload *)
-          mapM bs ~f:(fun (s, pld) -> liftPair2 s @@ payload_type pld)
-        in
-        pure @@ (TypedSyntax.Message typed_bs, (mk_qual_tp @@ msg_typ, rep))
+  (* Lift Ident (n, rep) to Ident (n, (Plain, rep)) *)
+  let add_plain_to_ident i =
+    match i with
+    | Ident (name, rep) -> Ident (name, (EMFR.Plain, rep))
+  
+  let rec mf_init_tag_pattern p =
+    match p with
+    | Wildcard -> MFSyntax.Wildcard
+    | Binder x -> MFSyntax.Binder (add_plain_to_ident x)
+    | Constructor (cn, ps) ->
+        MFSyntax.Constructor (
+          cn,
+          List.map mf_init_tag_pattern ps)
 
+  let mf_init_tag_payload p =
+    match p with
+    | MTag s -> MFSyntax.MTag s
+    | MLit l -> MFSyntax.MLit l
+    | MVar v -> MFSyntax.MVar (add_plain_to_ident v)
+  
+  let rec mf_init_tag_expr erep =
+    let (e, rep) = erep in
+    let res_e = 
+      match e with
+      | Literal l ->
+          MFSyntax.Literal l
+      | Var i ->
+          MFSyntax.Var (add_plain_to_ident i)
+      |  Fun (arg, t, body) ->
+          MFSyntax.Fun (
+              add_plain_to_ident arg,
+              t,
+              mf_init_tag_expr body)
+      | App (f, actuals) ->
+          MFSyntax.App (
+              add_plain_to_ident f, 
+              List.map add_plain_to_ident actuals)
+      | Builtin (i, actuals) ->
+          MFSyntax.Builtin (
+              add_plain_to_ident i,
+              List.map add_plain_to_ident actuals)
+      | Let (i, topt, lhs, rhs) ->
+          MFSyntax.Let (
+              add_plain_to_ident i,
+              topt,
+              mf_init_tag_expr lhs,
+              mf_init_tag_expr rhs)
+      | Constr (cname, ts, actuals) ->
+          MFSyntax.Constr (
+              cname,
+              ts,
+              List.map add_plain_to_ident actuals)
+      | MatchExpr (x, clauses) ->
+          MFSyntax.MatchExpr (
+              add_plain_to_ident x,
+              List.map (fun (p, e) ->
+                  (mf_init_tag_pattern p, mf_init_tag_expr e)) clauses)
+      | Fixpoint (f, t, body) ->
+          MFSyntax.Fixpoint (
+              add_plain_to_ident f,
+              t,
+              mf_init_tag_expr body)
+      | TFun (tvar, body) ->
+          MFSyntax.TFun (
+              add_plain_to_ident tvar,
+              mf_init_tag_expr body)
+      | TApp (tf, arg_types) ->
+          MFSyntax.TApp (
+              add_plain_to_ident tf,
+              arg_types)
+      | Message bs ->
+          MFSyntax.Message (
+              List.map (fun (s, p) -> (s, mf_init_tag_payload p)) bs) in
+    (res_e, (EMFR.Plain, rep))
+
+  let rec mf_init_tag_stmt srep =
+    let (s, rep) = srep in
+    let res_s = 
+      match s with
+      | Load (x, y) ->
+          MFSyntax.Load (
+            add_plain_to_ident x,
+            add_plain_to_ident y)
+      | Store (x, y) -> 
+          MFSyntax.Store (
+            add_plain_to_ident x,
+            add_plain_to_ident y)
+      | Bind (x, e) ->
+          MFSyntax.Bind (
+            add_plain_to_ident x,
+            mf_init_tag_expr e)
+      | MatchStmt (x, pss) ->
+          MFSyntax.MatchStmt (
+            add_plain_to_ident x,
+            List.map (fun (p, ss) ->
+                (mf_init_tag_pattern p,
+                 List.map mf_init_tag_stmt ss)) pss)
+      | ReadFromBC (x, s) ->
+          MFSyntax.ReadFromBC (
+            add_plain_to_ident x, s)
+      | AcceptPayment ->
+          MFSyntax.AcceptPayment
+      | SendMsgs x ->
+          MFSyntax.SendMsgs (add_plain_to_ident x)
+      | CreateEvnt x ->
+          MFSyntax.CreateEvnt (add_plain_to_ident x)
+      | Throw x ->
+          MFSyntax.Throw (add_plain_to_ident x) in
+    (res_s, rep)
+
+  let mf_init_tag_transition transition =
+    let { tname ; tparams ; tbody } = transition in
+    { MFSyntax.tname = tname;
+      MFSyntax.tparams =
+        List.map (fun (x, t) -> (add_plain_to_ident x, t)) tparams;
+      MFSyntax.tbody =
+        List.map mf_init_tag_stmt tbody }
+  
+  let mf_init_tag_contract contract =
+    let { cname ; cparams ; cfields ; ctrans } = contract in
+    { MFSyntax.cname = cname;
+      MFSyntax.cparams =
+        List.map (fun (x, t) -> (add_plain_to_ident x, t)) cparams;
+      MFSyntax.cfields =
+        List.map (fun (x, t, e) ->
+            (add_plain_to_ident x,
+             t,
+             mf_init_tag_expr e)) cfields;
+      MFSyntax.ctrans =
+        List.map mf_init_tag_transition ctrans }
+  
+  let mf_init_tag_library lib =
+    let { lname ; lentries } = lib in
+    { MFSyntax.lname = lname;
+      MFSyntax.lentries = List.map
+          (fun { lname ; lexp } ->
+             { MFSyntax.lname = add_plain_to_ident lname ;
+               MFSyntax.lexp = mf_init_tag_expr lexp }) lentries }
+  
+  let mf_init_tag_module cmod =
+    let { cname; libs; elibs; contr } = cmod in
+    let res_libs =
+      match libs with
+      | None -> None
+      | Some l -> Some (mf_init_tag_library l) in
+    { MFSyntax.cname = cname;
+      MFSyntax.libs = res_libs;
+      MFSyntax.elibs = elibs;
+      MFSyntax.contr = mf_init_tag_contract contr }
   
 end
-*)
+
