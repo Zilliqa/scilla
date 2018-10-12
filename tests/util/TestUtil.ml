75219a42b6fa4affadb340c0f06823ba9c6aa9f1
Vaivaswatha Nagaraj <vaivaswatha@zilliqa.com>
2018-10-12 09:01:09 +0530

    pass -jsonerrors in TestUtils when stdlib env is set

--- tests/util/TestUtil.ml
+++ tests/util/TestUtil.ml
@@ -89,12 +89,12 @@ module DiffBasedTests(Input : TestSuiteInput) = struct
         match Sys.getenv_opt GlobalConfig.StdlibTracker.scilla_stdlib_env with
         | Some _ -> false | None -> true
       in
-      let args = if use_stdlib then ["-libdir";libdir;"-jsonerrors";input_file] else [input_file] in
+      let args = if use_stdlib then ["-libdir";libdir;"-jsonerrors";input_file] else ["-jsonerrors";input_file] in
       (if (env.print_cli test_ctxt) then
         if use_stdlib then
           (Printf.printf "\nUsing CLI: %s %s %s %s %s\n" runner "-libdir" libdir "-jsonerrors" input_file)
         else
-          (Printf.printf "\nUsing CLI: %s %s\n" runner input_file));
+          (Printf.printf "\nUsing CLI: %s %s %s\n" runner "-jsonerrors" input_file));
       let update_gold = env.update_gold test_ctxt in
       if update_gold then
         assert_command ~exit_code:exit_code ~use_stderr:true ~foutput:output_updater ~chdir:dir ~ctxt:test_ctxt evalbin (args)
