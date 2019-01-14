671784dc8f63210f334e2aa48e2723c6fee32109
Vaivaswatha Nagaraj <vaivaswatha@zilliqa.com>
2019-01-14 22:39:06 +0530

    remove builtin dist, update tests to use library hash_dist

--- src/lang/base/BuiltIns.ml
+++ src/lang/base/BuiltIns.ml
@@ -809,29 +809,6 @@ module ScillaBuiltIns
     let ripemd160hash_type = tfun_typ "'A" @@ fun_typ (tvar "'A") (bystrx_typ address_length)
     let ripemd160hash ls _ = hash_helper ripemd160_hasher "ripemd160hash" address_length ls
 
-    let big_int_of_hash h =
-      let s = match Hex.of_string h with
-        | `Hex s -> s
-      in Big_int.big_int_of_string s
-
-    (* TODO: define for other ByStrX types? *)
-    let dist_type = fun_typ (bystrx_typ hash_length) @@ fun_typ (bystrx_typ hash_length) uint128_typ
-    let dist_arity = 2    
-    let dist ls _ = match ls with
-      | [ByStrX(_, x); ByStrX(_, y)] ->
-          let i1 = big_int_of_hash x in
-          let i2 = big_int_of_hash y in
-          let two256 = power_big_int_positive_big_int (big_int_of_int 2) (big_int_of_int 256) in
-          let i1' = mod_big_int i1 two256 in
-          let i2' = mod_big_int i2 two256 in
-          let dist = abs_big_int (sub_big_int i1' i2') in
-          let i = build_prim_literal uint256_typ (string_of_big_int dist) in
-          (match i with
-           | Some ui -> pure ui
-           | None -> builtin_fail "Crypto.dist: Error building Uint256 from hash distance" ls
-          )
-      | _ -> builtin_fail "Crypto.dist" ls
-
     (* ByStrX -> ByStr *)
     let to_bystr_type = tfun_typ "'A" @@ fun_typ (tvar "'A") bystr_typ
     let to_bystr_arity = 1
@@ -1121,7 +1098,6 @@ module ScillaBuiltIns
 
       (* Crypto *)
       ("eq", Crypto.eq_arity, Crypto.eq_type, Crypto.eq_elab, Crypto.eq);
-      ("dist", Crypto.dist_arity, Crypto.dist_type, elab_id , Crypto.dist);
       ("to_uint256", Crypto.to_uint256_arity, Crypto.to_uint256_type, Crypto.to_uint256_elab, Crypto.to_uint256);
       ("sha256hash", Crypto.hash_arity, Crypto.hash_type,Crypto.hash_elab, Crypto.sha256hash);
       ("keccak256hash", Crypto.hash_arity, Crypto.hash_type,Crypto.hash_elab, Crypto.keccak256hash);
