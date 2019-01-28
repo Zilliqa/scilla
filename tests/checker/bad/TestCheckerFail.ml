f257e52bbc91474cb863f496ce5d0106533fd6b1
Jacob Johannsen <jajocnn@gmail.com>
2019-01-28 21:35:11 +0100

    Make ADTs with unstorable constructor arguments unstorable

--- tests/checker/bad/TestCheckerFail.ml
+++ tests/checker/bad/TestCheckerFail.ml
@@ -49,6 +49,7 @@ module Tests = TestUtil.DiffBasedTests(
       "bad_adt_2.scilla";
       "bad_adt_3.scilla";
       "bad_adt_4.scilla";
+      "unstorable_adt.scilla";
     ]
     let exit_code : Unix.process_status = WEXITED 1
   end)
