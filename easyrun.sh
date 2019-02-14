7de4e60b9320742f8a3aa32cad0256044a56709a
Vaivaswatha Nagaraj <vaivaswatha@zilliqa.com>
2019-02-14 10:20:06 +0530

    update easyrun.sh

--- easyrun.sh
+++ easyrun.sh
@@ -2,9 +2,9 @@
 
 function print_available_contracts
 {
-    for ff in tests/contracts/*
+    for ff in tests/runner/*
     do
-        if [[ -d $ff && $ff != "tests/contracts/other" ]]
+        if [[ -d $ff ]]
         then
             f=`basename $ff`
             echo -n "$f|"
@@ -29,15 +29,16 @@ fi
 contract=$1
 i=$2
 
-cdir="tests/contracts/$contract"
+sdir="tests/contracts"
+cdir="tests/runner/$contract"
 
-if [[ ! -d ${cdir} || ! -f ${cdir}/state_${i}.json ]]
+if [[ ! -d ${cdir} || ! -f ${sdir}/${contract}.scilla || ! -f ${cdir}/state_${i}.json ]]
 then
     echo "Test $contract $i does not exist"
     print_usage_and_exit
 fi
 
-./bin/scilla-runner -init ${cdir}/init.json -istate ${cdir}/state_${i}.json -imessage ${cdir}/message_${i}.json -o ${cdir}/output_${i}.json -iblockchain ${cdir}/blockchain_${i}.json -i ${cdir}/contract.scilla -libdir src/stdlib -gaslimit 8000
+./bin/scilla-runner -init ${cdir}/init.json -istate ${cdir}/state_${i}.json -imessage ${cdir}/message_${i}.json -o ${cdir}/output_${i}.json -iblockchain ${cdir}/blockchain_${i}.json -i ${sdir}/${contract}.scilla -libdir src/stdlib -gaslimit 8000
 
 status=$?
 
