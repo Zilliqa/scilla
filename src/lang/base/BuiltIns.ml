7506a6a5edfbea2ffc24f37b9639cbf88c3300a9
Anton Trunov <anton@zilliqa.com>
2019-04-23 18:26:20 +0300

    More stable hashing scheme for complex literals

--- src/lang/base/BuiltIns.ml
+++ src/lang/base/BuiltIns.ml
@@ -628,15 +628,28 @@ module ScillaBuiltIns
 
     let hash_helper hasher name len ls = match ls with
       | [l] ->
-          let lstr =
-            (match l with
+          let rec raw_bytes l = match l with
             | StringLit s -> s
             | IntLit il -> bstring_from_int_lit il
             | UintLit uil -> bstring_from_uint_lit uil
+            | BNum s -> s
             | ByStr s | ByStrX (_, s) -> fromhex s
-            (* Anything else, just serialize with SExp. *)
-            | _ -> sexp_of_literal l |> Sexplib.Sexp.to_string) in
-          let lhash = hasher lstr in
+            | Msg entries ->
+                let raw_entries = List.map entries ~f:(fun (s, v) -> s ^ raw_bytes v) in
+                Core.String.concat ~sep:"" raw_entries
+            (* XXX: in case of unbearable performance overhead, improve the following *)
+            | Map ((kt, vt), tbl) ->
+                let alist = Caml.Hashtbl.fold (fun k v acc -> (k, v) :: acc) tbl [] in
+                let sorted_alist = List.sort ~compare alist in
+                let kv_raw_bytes = List.map sorted_alist ~f:(fun (k,v) -> raw_bytes k ^ raw_bytes v) in
+                pp_typ kt ^ pp_typ vt ^ Core.String.concat ~sep:"" kv_raw_bytes
+            | ADTValue (cons_name, _, params) ->
+                let raw_params = List.map params ~f:raw_bytes in
+                Core.String.concat ~sep:"" (cons_name :: raw_params)
+            | Clo _fun -> "(Clo <fun>)"
+            | TAbs _fun -> "(Tabs <fun>)"
+          in
+          let lhash = hasher (raw_bytes l) in
           let lhash_hex = tohex lhash in
           let lo = build_prim_literal (Bystrx_typ len) lhash_hex in
           (match lo with
