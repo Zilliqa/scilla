514a0a447b172cd835baa95f51e759b45ecf9e1b
Anton Trunov <anton@zilliqa.com>
2019-04-23 19:03:06 +0300

    Remove type information from raw map representation

--- src/lang/base/BuiltIns.ml
+++ src/lang/base/BuiltIns.ml
@@ -629,6 +629,7 @@ module ScillaBuiltIns
     let hash_helper hasher name len ls = match ls with
       | [l] ->
           let rec raw_bytes l = match l with
+          (* we don't keep type information here *)
             | StringLit s -> s
             | IntLit il -> bstring_from_int_lit il
             | UintLit uil -> bstring_from_uint_lit uil
@@ -638,11 +639,11 @@ module ScillaBuiltIns
                 let raw_entries = List.map entries ~f:(fun (s, v) -> s ^ raw_bytes v) in
                 Core.String.concat ~sep:"" raw_entries
             (* XXX: in case of unbearable performance overhead, improve the following *)
-            | Map ((kt, vt), tbl) ->
+            | Map (_, tbl) ->
                 let alist = Caml.Hashtbl.fold (fun k v acc -> (k, v) :: acc) tbl [] in
                 let sorted_alist = List.sort ~compare alist in
                 let kv_raw_bytes = List.map sorted_alist ~f:(fun (k,v) -> raw_bytes k ^ raw_bytes v) in
-                pp_typ kt ^ pp_typ vt ^ Core.String.concat ~sep:"" kv_raw_bytes
+                Core.String.concat ~sep:"" kv_raw_bytes
             | ADTValue (cons_name, _, params) ->
                 let raw_params = List.map params ~f:raw_bytes in
                 Core.String.concat ~sep:"" (cons_name :: raw_params)
