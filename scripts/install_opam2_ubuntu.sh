de739674ef9242a601821f369d44212a3a158afb
Anton Trunov <anton@zilliqa.com>
2019-04-29 21:28:14 +0300

    fix downloading opam executable

--- scripts/install_opam2_ubuntu.sh
+++ scripts/install_opam2_ubuntu.sh
@@ -7,6 +7,7 @@ set -ev
 # ./scripts/build_openssl.sh
 
 # install opam 2.0 -- the current Ubuntu versions available on Travis CI are v1.x
-wget https://github.com/ocaml/opam/releases/download/2.0.4/opam-2.0.4-x86_64-linux
-sudo mv opam-2.0.0-x86_64-linux /usr/local/bin/opam
+# -O option does not exactly work as "save as", but it does the job in this case
+wget https://github.com/ocaml/opam/releases/download/2.0.4/opam-2.0.4-x86_64-linux -O opam
+sudo mv ./opam /usr/local/bin/opam
 sudo chmod a+x /usr/local/bin/opam
