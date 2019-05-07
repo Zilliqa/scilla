141d0420d1780659d4f4a800c9ed7e58b25c254f
Jacob Johannsen <jajocnn@gmail.com>
2019-05-07 09:49:19 +0200

    Positive scoping tests for procedures

--- tests/checker/good/TestCheckerSuccess.ml
+++ tests/checker/good/TestCheckerSuccess.ml
@@ -84,6 +84,8 @@ module CheckerTests = TestUtil.DiffBasedTests(
       "TestLibNS4.scillib";
       "libdiamond2.scilla";
       "map-inplace-update-with-_sender.scilla";
+(*      "backward_definition_procedure.scilla";
+        "global_scope_procedures.scilla"; *)
     ]
     let exit_code : Unix.process_status = WEXITED 0
   end)
