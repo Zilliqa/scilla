d9410422e207c0ec76deccccba7a7660d01cc3f9
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2019-11-11 17:14:07 +0300

    Update shell.nix

--- shell.nix
+++ shell.nix
@@ -1,30 +1,39 @@
+{ compiler ? "4.06" }:
+
 with import <nixpkgs> { };
 
 let
-  opkgs = pkgs.ocamlPackages;
+  ver = with lib.strings; concatStringsSep "_" (splitString "." compiler);
+  opkgs = pkgs.ocaml-ng."ocamlPackages_${ver}";
   ppx_deriving_protobuf = opkgs.callPackage ./scripts/nix/ppx_deriving_protobuf.nix { };
   ocaml-protoc = opkgs.callPackage ./scripts/nix/ocaml-protoc.nix { inherit ppx_deriving_protobuf; };
   rpclib = opkgs.callPackage ./scripts/nix/rpclib.nix { };
   ppx_deriving_rpc = opkgs.callPackage ./scripts/nix/ppx_deriving_rpc.nix { inherit rpclib; };
-  ocamlVersion = opkgs.ocaml.version;
   systemPkgs = [
-    m4 ocaml cmake opam gcc dune ncurses boost openssl
-    zlib gmp procps secp256k1 libffi pkgconfig pcre patdiff
+    m4 cmake gmp procps opam gcc ncurses boost openssl
+    zlib secp256k1 libffi pkgconfig pcre patdiff
   ];
   ocamlPkgs = with opkgs; [
-    base core core_bench core_profiler ppx_deriving ppx_tools_versioned
-    ppx_sexp_conv bisect_ppx fileutils hex stdint batteries zarith cryptokit atdgen
-    bitstring ctypes findlib utop angstrom ounit expect_test_helpers patience_diff
-    ocaml_pcre merlin ocp-indent ocp-index yojson menhir secp256k1 rpclib re2 ppx_let
+    ocaml dune ocamlbuild findlib utop merlin ocp-indent ocp-index
+    base core core_bench
+    textutils ppx_sexp_conv
+    ppx_deriving ppx_tools_versioned
+    bisect_ppx fileutils hex stdint batteries zarith cryptokit atdgen
+    bitstring ctypes angstrom ounit expect_test_helpers patience_diff
+    ocaml_pcre yojson menhir secp256k1 rpclib re2 ppx_let
     ppx_deriving_protobuf ocaml-protoc ppx_deriving_rpc result rresult xmlm
   ];
   packages = systemPkgs ++ ocamlPkgs;
-  mkpath = p: "${p}/lib/ocaml/${ocamlVersion}/site-lib";
+  mkpath = p: "${p}/lib/ocaml/${opkgs.ocaml.version}/site-lib";
   paths = builtins.concatStringsSep ":" (map mkpath ocamlPkgs);
   siteLisp = "share/emacs/site-lisp";
-in pkgs.mkShell {
+in
+with opkgs; pkgs.mkShell {
   buildInputs = packages;
   shellHook = with opkgs; ''
     export OCAML_TOPLEVEL_PATH="${mkpath findlib}"
+    export UTOP_SITE_LISP="${utop}/${siteLisp}";
+    export MERLIN_SITE_LISP="${merlin}/${siteLisp}";
+    export OCP_INDENT_SITE_LISP="${ocp-indent}/${siteLisp}";
   '';
 }
