d49d85c9176480d05b6a57dad1b3176f6ce38d2d
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2019-11-13 15:49:22 +0300

    Update run_benchmarks script

--- scripts/run_benchmarks.sh
+++ scripts/run_benchmarks.sh
@@ -11,28 +11,18 @@ check_variable () {
   fi
 }
 
-check_variable "WORKSPACE"
-
-working_dir="${WORKSPACE%@*}/travis"
-
 if [ -n "$BENCH_DEBUG" ]
 then
    echo "DEBUG: ocaml -version = $(ocaml -version)"
-   echo "DEBUG: working_dir = $working_dir"
    echo "DEBUG: TRAVIS_COMMIT = $TRAVIS_COMMIT"
 fi
 
-rm -rf "$working_dir"
-mkdir -p "$working_dir"
-
-scilla_dir="$working_dir/scilla"
-git clone -q https://github.com/Zilliqa/scilla.git "$scilla_dir"
-cd "$scilla_dir"
-
 git checkout -q "$TRAVIS_COMMIT"
 
+make
 dune exe ./bench/bin/scilla_bench_runner.exe -- -ci
 
-git checkout -q master
+git checkout -q perf-benchmarks
 
+make
 dune exe ./bench/bin/scilla_bench_runner.exe -- -ci
