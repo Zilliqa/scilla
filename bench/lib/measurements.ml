7cdacd942f7fefce79a8fc9ae465ce42a78f28c1
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2019-11-14 13:03:06 +0300

    Remove unused functions from Measurements module

--- bench/lib/measurements.ml
+++ bench/lib/measurements.ml
@@ -23,38 +23,6 @@ let result_to_option = function
       None
   | Ok r -> Some r
 
-let load_from path =
-  path
-  |> Sys.ls_dir
-  |> List.map ~f:(fun fn ->
-      let filename = path ^/ fn in
-      B.Measurement.load ~filename)
-
-let load ~dir ~current_dir ~env =
-  let open Env in
-  let mk_path dir = env.results_dir ^/ dir in
-  let path = Storage.latest ~timestamp:dir ~current:current_dir ~env in
-  Option.map path ~f:(fun s ->
-      load_from (mk_path s), s)
-
-let save_one m ~path =
-  let name = m |> B.Measurement.name |> Util.sanitize in
-  let filename = path ^/ name ^. "txt" in
-  B.Measurement.save m ~filename
-
-let save meas ~env =
-  (* Save the measurements in a more
-     appropriate place in the file system *)
-  let dir = Timestamp.mk () in
-  let path = env.Env.results_dir ^/ dir in
-  printf "Measurements will be saved to %s.\n%!" dir;
-  (* Create a directory to place measurements into,
-     use the current timestamp as a name for it *)
-  Unix.mkdir path;
-  (* Save each measurement as a separate file *)
-  List.iter meas ~f:(save_one ~path);
-  dir
-
 (* Run the [B.Analysis.analyze] for each
    measurement and get back the results *)
 let analyze meas =
--- bench/lib/measurements.mli
+++ bench/lib/measurements.mli
@@ -14,21 +14,5 @@
 
 open Core_bench
 
-(** Save benchmark measurements, return
-    the directory where it was saved. *)
-val save : Measurement.t list -> env:Env.t -> string
-
-(** Load measurements from the specified [dir].
-    If the [dir] is not given then previous measurements will
-    be loaded by finding the directory named after the
-    latest timestamp, which is not the same as the [current_dir].
-    Return the [Measurement.t list] along with
-    used timestamp (directory name) represented as a [string]. *)
-val load
-  :  dir:string option
-  -> current_dir:string option
-  -> env:Env.t
-  -> (Measurement.t list * string) option
-
 (** Analyze benchmark measurements. *)
 val analyze : Measurement.t list -> Analysis_result.t list
