daef40bb3208a1c332692c4f16e3d604521d3793
Anton Trunov <anton@zilliqa.com>
2019-12-09 00:05:04 +0300

    Add Utils.mli and separate AssocDictionary

--- src/lang/base/Utils.mli
+++ src/lang/base/Utils.mli
@@ -2,36 +2,28 @@
   This file is part of scilla.
 
   Copyright (c) 2018 - present Zilliqa Research Pvt. Ltd.
-  
+
   scilla is free software: you can redistribute it and/or modify it under the
   terms of the GNU General Public License as published by the Free Software
   Foundation, either version 3 of the License, or (at your option) any later
   version.
- 
+
   scilla is distributed in the hope that it will be useful, but WITHOUT ANY
   WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
   A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
- 
+
   You should have received a copy of the GNU General Public License along with
   scilla.  If not, see <http://www.gnu.org/licenses/>.
 *)
 
-open Core_kernel
-
-(* Add item a to list if it isn't already present. Use ~equal to check presence. *)
-let list_add_unique ~equal ls a =
-  if Core.List.mem ls a ~equal then ls else (a :: ls)
-
-(* Fold n times, each time applying 0-(n-1) and accummulator to f. *)
-let int_fold ~init ~(f : 'a -> int -> 'a) n =
-  let rec recurser acc i =
-    if i = n then acc else
-    let acc' = f acc i in
-    recurser acc' (i+1)
-  in
-  recurser init 0
-
+open Core
 open ErrorUtils
-exception InternalError of scilla_error list
-let mk_internal_error msg = InternalError (mk_error0 msg)
 
+(* Add to list only if unique considering equal *)
+val list_add_unique :
+  equal:('a -> 'a -> bool) -> 'a List.t -> 'a -> 'a List.t
+
+val int_fold : init:'a -> f:('a -> int -> 'a) -> int -> 'a
+
+exception InternalError of scilla_error list
+val mk_internal_error : string -> exn
