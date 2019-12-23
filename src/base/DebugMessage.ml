855bef71bdcdabb854a660e87d96085d2532f08c
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2019-12-23 18:16:47 +0300

    Revert DebugMessage module (remove calls to flash)

--- src/base/DebugMessage.ml
+++ src/base/DebugMessage.ml
@@ -23,18 +23,18 @@ open Core_kernel
 (* Prints to log file *)
 let plog msg =
   if get_debug_level () <> Debug_None then
-  let fname = get_log_file () in
-  Out_channel.with_file fname ~append:true
-    ~f:(fun h -> Out_channel.(output_string h msg; flush h))
+    let fname = get_log_file () in
+    Out_channel.with_file fname ~append:true
+      ~f:(fun h -> Out_channel.output_string h msg;)
 
 (* Prints to stdout and log file *)
 let pout msg =
-  Out_channel.(output_string stdout msg; flush stdout);
+  Out_channel.output_string stdout msg;
   plog ("stdout: " ^ msg ^ "\n")
 
 (* Prints to stderr and log file *)
 let perr msg =
-  Out_channel.(output_string stderr msg; flush stderr);
+  Out_channel.output_string stderr msg;
   plog ("stderr: " ^ msg ^ "\n")
 
 (* Prints to trace file, if set, else to stdout. *)
@@ -42,7 +42,7 @@ let ptrace msg =
   let fname = GlobalConfig.get_trace_file() in
   if fname <> ""
   then
-    Out_channel.(with_file fname ~append:true
-      ~f:(fun h -> output_string h msg; flush h))
+    Out_channel.with_file fname ~append:true
+      ~f:(fun h -> Out_channel.output_string h msg)
   else
-    Out_channel.(output_string stdout msg; flush stdout)
+    Out_channel.output_string stdout msg
