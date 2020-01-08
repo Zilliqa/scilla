ff5a0bcf2e56b58d4d10a1d0540a973e214eabe9
Vaivaswatha Nagaraj <vaivaswatha@zilliqa.com>
2020-01-08 18:38:10 +0530

    More minor fixes

--- src/runners/scilla_runner.ml
+++ src/runners/scilla_runner.ml
@@ -115,7 +115,7 @@ let rec output_event_json elist =
     | _ -> `Null :: j)
   | [] -> []
 
-let validate_get_init_json init_file gas_remaining sourcever =
+let validate_get_init_json init_file gas_remaining source_ver =
   (* Retrieve initial parameters *)
   let initargs =
     try
@@ -127,13 +127,13 @@ let validate_get_init_json init_file gas_remaining sourcever =
   (* Check for version mismatch. Subtract penalty for mismatch. *)
   let emsg = mk_error0 ("Scilla version mismatch\n") in
   let rgas = Uint64.sub gas_remaining (Uint64.of_int Gas.version_mismatch_penalty) in
-  let init_json_scilla_version = List.Assoc.find initargs ~equal:(=) ContractUtil.scilla_version_label in
+  let init_json_scilla_version = List.Assoc.find initargs ~equal:String.equal ContractUtil.scilla_version_label in
   let _ =
     match init_json_scilla_version with
     | Some (UintLit (Uint32L v)) ->
       let (mver, _, _) = scilla_version in
       let v' = Uint32.to_int v in
-      if v' <> mver || mver <> sourcever
+      if v' <> mver || mver <> source_ver
       then fatal_error_gas emsg rgas
     | _ -> fatal_error_gas emsg rgas
   in
