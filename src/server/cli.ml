f4762c49cdaf236ef75c963918420abce7822f67
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2020-02-04 14:13:36 +0300

    Fix typo

--- src/server/cli.ml
+++ src/server/cli.ml
@@ -26,7 +26,7 @@ module Cmd = struct
       info "server" ~version ~doc )
 
   let server ~sock_path ~num_pending =
-    ( const @@ Server.start ~sock_path ~num_pending,
+    ( const @@ (fun () -> Server.start ~sock_path ~num_pending) $ const (),
       info "start" ~doc:"Start Scilla server" )
 end
 
