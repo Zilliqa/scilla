6dfb8920d20332b90da3702fe2128fdd67912d23
George Pîrlea <george@dranov.ro>
2020-03-17 17:53:34 +0000

    Keep track of transition parameter shadowing
    All keys in a component summary Read or Write operation are now
    guaranteed to refer to actual transition parameters. (Before this
    change, they could refer to bindings with the same name, i.e.
    bindings that shadowed the parameter.)

--- src/base/ShardingAnalysis.ml
+++ src/base/ShardingAnalysis.ml
@@ -38,6 +38,9 @@ struct
   module TU = TypeUtilities
   open SASyntax
 
+  (* For each contract component, we keep track of the operations it performs.
+  This gives us enough information to tell whether two transitions have disjoint
+  footprints and thus commute. *)
   type component_operation =
     (* Read of cfield, with map keys which are comp_params if field is a map *)
     | Read of ER.rep ident *  ER.rep ident list option
@@ -72,13 +75,24 @@ struct
       let str_b = sprint_operation b in
       compare str_a str_b
   end
+  (* A component's summary is the set of the operations it performs *)
   module ComponentSummary = Set.Make(OrderedComponentOperation)
-
-  type signature = ComponentSummary
 
   let sprint_summary summ =
     let ops = ComponentSummary.elements summ in
     List.fold_left (fun acc op -> acc ^ sprint_operation op ^ "\n") "" ops
+
+  type component_summary = ComponentSummary
+
+  (* We keep track of whether identifiers in the impure part of the language
+  shadow any of their component's parameters *) type ident_shadow_status =
+    | ShadowsComponentParameter
+    | ComponentParameter
+    | DoesNotShadow
+
+  type signature =
+    | ComponentSig of component_summary
+    | IdentSig of ident_shadow_status
 
   module SAEnv = struct
     open AssocDictionary
@@ -91,6 +105,9 @@ struct
 
     (* In env, add mapping id => s *)
     let addS env id s = insert id s env
+
+    (* Return None if key doesn't exist *)
+    let lookupS env id = lookup id env
 
     (* In env, resolve id => s and return s (fails if cannot resolve). *)
     let resolvS ?(lopt = None) env id =
@@ -126,10 +143,10 @@ struct
         "" l
   end
 
-  let identity_bind_ident_list senv idlist =
+  let env_bind_ident_list senv idlist (sgn : signature) =
     List.fold_left
       (fun acc_senv i ->
-        SAEnv.addS acc_senv (get_id i) ComponentSummary.empty)
+        SAEnv.addS acc_senv (get_id i) sgn)
       senv idlist
 
   let is_bottom_level_access m klist =
@@ -138,12 +155,52 @@ struct
     let map_access = nindices <  TU.map_depth mt in
     not map_access
 
+  let all_keys_are_parameters senv klist =
+    let is_component_parameter k senv =
+      let m = SAEnv.lookupS senv (get_id k) in
+      match m with
+      | None -> false
+      | Some m_sig -> m_sig = (IdentSig ComponentParameter)
+    in
+    List.for_all (fun k -> is_component_parameter k senv) klist
+
+  let map_access_can_be_summarised senv m klist =
+    is_bottom_level_access m klist && all_keys_are_parameters senv klist
+
+  (* Precondition: senv contains the component parameters, appropriately marked *)
   let rec sa_stmt senv summary (stmts : stmt_annot list) =
+    (* Add a new identifier to the environment, keeping track of whether it
+    shadows a component parameter *)
+    let env_new_ident i senv =
+      let id = get_id i in
+      let opt_shadowed = SAEnv.lookupS senv id in
+      let new_id_sig = match opt_shadowed with
+        | None -> (IdentSig DoesNotShadow)
+        | Some shadowed_sig ->
+            match shadowed_sig with
+            | IdentSig ComponentParameter -> IdentSig ShadowsComponentParameter
+            | _ -> IdentSig DoesNotShadow
+      in
+        SAEnv.addS senv id new_id_sig
+    in
+
     (* Helpers to continue after accumulating an operation *)
-    let cont summary sts = sa_stmt senv summary sts in
+    let cont senv summary sts = sa_stmt senv summary sts in
+    (* Perform an operation *)
     let cont_op op summary sts =
       let summary' = ComponentSummary.add op summary in
-      cont summary' sts
+      cont senv summary' sts
+    in
+    (* Introduce a new identifier *)
+    let cont_ident ident summary sts =
+      let senv' =  env_new_ident ident senv in
+      cont senv' summary sts
+    in
+    (* Perform an operation and introduce an identifier *)
+    let cont_ident_op ident op summary sts =
+      let senv' =  env_new_ident ident senv in
+      let summary' = ComponentSummary.add op summary in
+      cont senv' summary' sts
     in
 
     (* We can assume everything is well-typed, which makes this much easier *)
@@ -151,28 +208,38 @@ struct
     | [] -> summary
     | (s, sloc) :: sts -> (
         match s with
-        | Load (_, f) -> cont_op (Read (f, None)) summary sts
-        | Store (_, f) -> cont_op (Write (f, None)) summary sts
+        | Load (x, f) -> cont_ident_op x (Read (f, None)) summary sts
+        | Store (f, _) -> cont_op (Write (f, None)) summary sts
         | MapGet (x, m, klist, _) ->
-            let op = if is_bottom_level_access m klist
+            let op = if map_access_can_be_summarised senv m klist
             then (Read (m, Some klist)) else AlwaysExclusive in
-            cont_op op summary sts
+            cont_ident_op x op summary sts
         | MapUpdate (m, klist, _) ->
-            let op = if is_bottom_level_access m klist
+            let op = if map_access_can_be_summarised senv m klist
             then (Write (m, Some klist)) else AlwaysExclusive in
             cont_op op summary sts
         | AcceptPayment -> cont_op AcceptMoney summary sts
         | SendMsgs i -> cont_op SendMessages summary sts
 
+        | Bind (x, _) | ReadFromBC (x, _) -> cont_ident x summary sts
+
         | MatchStmt (x, clauses) ->
-          let summarize_clause (pattern, cl_sts) = sa_stmt senv summary cl_sts in
+          let summarize_clause (pattern, cl_sts) =
+            let binders = get_pattern_bounds pattern in
+            let senv' = List.fold_left
+              (fun env_acc id -> env_new_ident id env_acc) senv binders
+            in
+            sa_stmt senv' summary cl_sts
+          in
           let cl_summaries = List.map summarize_clause clauses in
           let summary' =
               List.fold_left
                 (fun acc_summ cl_summ -> ComponentSummary.union acc_summ cl_summ) summary cl_summaries
           in
-          cont summary' sts
-        | _ -> cont summary sts
+          cont senv summary' sts
+
+        (* TODO: be defensive about unsupported instructions *)
+        | _ -> cont senv summary sts
     )
 
   let sa_component_summary senv (comp : component) =
@@ -186,8 +253,10 @@ struct
       ]
       @ comp.comp_params
     in
+    (* Add component parameters to the analysis environment *)
     let senv' =
-      identity_bind_ident_list senv (List.map (fun (i, _) -> i) all_params)
+      env_bind_ident_list senv (List.map (fun (i, _) -> i) all_params)
+        (IdentSig ComponentParameter)
     in
     sa_stmt senv' ComponentSummary.empty comp.comp_body
 
