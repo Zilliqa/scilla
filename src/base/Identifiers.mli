c5bef2629ddcfd2df5b5a6ace53d53cd9e8fa74a
Jacob Johannsen <jajocnn@gmail.com>
2020-04-08 00:41:36 +0200

    Copyright year changed

--- src/base/Identifiers.mli
+++ src/base/Identifiers.mli
@@ -1,7 +1,7 @@
 (*
   This file is part of scilla.
 
-  Copyright (c) 2018 - present Zilliqa Research Pvt. Ltd.
+  Copyright (c) 2020 - present Zilliqa Research Pvt. Ltd.
   
   scilla is free software: you can redistribute it and/or modify it under the
   terms of the GNU General Public License as published by the Free Software
