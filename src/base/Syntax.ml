dcd88cf06062f0da7671a16f1805b009fff9dd7d
Vaivaswatha Nagaraj <vaivaswatha@zilliqa.com>
2020-04-20 10:01:02 +0530

    Prepare for v0.7.0 release

--- src/base/Syntax.ml
+++ src/base/Syntax.ml
@@ -27,7 +27,7 @@ open Literal
 exception SyntaxError of string * loc
 
 (* Version of the interpreter (major, minor, patch) *)
-let scilla_version = (0, 6, 0)
+let scilla_version = (0, 7, 0)
 
 let address_length = 20
 
