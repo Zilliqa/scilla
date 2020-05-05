f3ccc725e2fe3bd0435a460a587e22d7ba1aca05
Vaivaswatha N <vaivaswatha@users.noreply.github.com>
2020-05-05 21:16:14 +0530

    Provide a public name for scilla_test to make it installable (#837)
    * Provide a public name for scilla_test to make it installable
    
    * Update README to include opam installation instructions
    
    * Minor fix to install_opam2_ubuntu.sh

--- scripts/install_opam2_ubuntu.sh
+++ scripts/install_opam2_ubuntu.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 
-# The -x flag makes sure the script exits as soon as one command returns a non-zero exit code
+# The -e flag makes sure the script exits as soon as one command returns a non-zero exit code
 # The -v flag makes the shell print all lines in the script before executing them, which helps identify which steps failed
 set -ev
-
-# ./scripts/build_openssl.sh
 
 # install opam 2.0 -- the current Ubuntu versions available on Travis CI are v1.x
 # -O option does not exactly work as "save as", but it does the job in this case
