9af94d768ce2937c462351bc9d8cf02021a7bb76
Jacob Johannsen <jajocnn@gmail.com>
2020-05-29 19:39:22 +0200

    Fix copyright notice for code files

--- misc/emacs-mode/scilla-mode.el
+++ misc/emacs-mode/scilla-mode.el
@@ -1,3 +1,19 @@
+;;  This file is part of scilla.
+;;
+;;  Copyright (c) 2018 - present Zilliqa Research Pvt. Ltd.
+;;  
+;;  scilla is free software: you can redistribute it and/or modify it under the
+;;  terms of the GNU General Public License as published by the Free Software
+;;  Foundation, either version 3 of the License, or (at your option) any later
+;;  version.
+;; 
+;;  scilla is distributed in the hope that it will be useful, but WITHOUT ANY
+;;  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+;;  A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+;; 
+;;  You should have received a copy of the GNU General Public License along with
+;;  scilla.  If not, see <http://www.gnu.org/licenses/>.
+
 ;; This is an Emacs major mode for the Scilla language.
 ;; Include the below lines (with path set properly) in ~/.emacs
 ;;    ;; For enabling flycheck mode for Scilla.
--- scripts/libff.sh
+++ scripts/libff.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/env bash
 
+##  This file is part of scilla.
+##
+##  Copyright (c) 2018 - present Zilliqa Research Pvt. Ltd.
+##  
+##  scilla is free software: you can redistribute it and/or modify it under the
+##  terms of the GNU General Public License as published by the Free Software
+##  Foundation, either version 3 of the License, or (at your option) any later
+##  version.
+## 
+##  scilla is distributed in the hope that it will be useful, but WITHOUT ANY
+##  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+##  A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+## 
+##  You should have received a copy of the GNU General Public License along with
+##  scilla.  If not, see <http://www.gnu.org/licenses/>.
+
 # This script is expected to be executed from Scilla root
 # $./scripts/libff.sh
 # The script builds and installs libff in the
