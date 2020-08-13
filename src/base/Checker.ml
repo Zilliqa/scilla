f57fa9c6f632c5d8923aafb4c3edfcaf47a13f43
Vaivaswatha Nagaraj <vaivaswatha@zilliqa.com>
2020-08-13 14:13:08 +0530

    Add missing refactoring missed out in previous commit

--- src/base/Checker.ml
+++ src/base/Checker.ml
@@ -301,11 +301,7 @@ let check_cmodule cli =
       else None
     in
     let remaining_gas' =
-      let remain = Uint64.div remaining_gas Gas.scale_factor in
-      (* Ensure that at least one unit of gas is consumed. *)
-      if Uint64.compare remain cli.gas_limit = 0 then
-        Uint64.sub remain Uint64.one
-      else remain
+      Gas.finalize_remaining_gas cli.gas_limit remaining_gas
     in
     pure @@ (cmod, tenv, event_info, type_info, cf_info_opt, remaining_gas')
   in
