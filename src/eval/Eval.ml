3b844e76f21fcdb1602d4a54b7bd1efb73260151
Vaivaswatha Nagaraj <vaivaswatha@zilliqa.com>
2020-10-15 19:08:18 +0530

    make fmt

--- src/eval/Eval.ml
+++ src/eval/Eval.ml
@@ -540,7 +540,8 @@ let init_libraries clibs elibs =
   DebugMessage.plog "Loading library types and functions.";
   let%bind rec_env =
     let%bind rlibs =
-      mapM ~f:(Fn.compose fromR (EvalGas.lib_entry_cost))
+      mapM
+        ~f:(Fn.compose fromR EvalGas.lib_entry_cost)
         RecursionPrinciples.recursion_principles
     in
     init_lib_entries (pure Env.empty) rlibs
