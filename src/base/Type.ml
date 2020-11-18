0881fc6e70789f32ab30719617b93c995e5693dd
Vaivaswatha Nagaraj <vaivaswatha@zilliqa.com>
2020-11-18 09:18:28 +0530

    address review comments

--- src/base/Type.ml
+++ src/base/Type.ml
@@ -343,10 +343,8 @@ module MkType (I : ScillaIdentifier) = struct
   let bystrx_typ b = PrimType (Bystrx_typ b)
 
   let int_width = function
-    | PrimType (Int_typ Bits32) | PrimType (Uint_typ Bits32) -> Some 32
-    | PrimType (Int_typ Bits64) | PrimType (Uint_typ Bits64) -> Some 64
-    | PrimType (Int_typ Bits128) | PrimType (Uint_typ Bits128) -> Some 128
-    | PrimType (Int_typ Bits256) | PrimType (Uint_typ Bits256) -> Some 256
+    | PrimType (Int_typ bits) | PrimType (Uint_typ bits) ->
+        Some (PrimType.int_bit_width_to_int bits)
     | _ -> None
 
   (* Given a ByStrX string, return integer X *)
