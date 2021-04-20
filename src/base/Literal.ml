ab7344b2e1b05810623d67b144409d6ed8a2b3b9
Jacob Johannsen <jajocnn@gmail.com>
2021-04-20 21:17:24 +0200

    Fix issue with +/- 0

--- src/base/Literal.ml
+++ src/base/Literal.ml
@@ -417,18 +417,18 @@ module MkLiteral (T : ScillaType) = struct
             let x_no_leading_plus = String.drop_prefix x 1 in
             (* Strip all leading 0s *)
             Str.replace_first
-              (Str.regexp "^0*\\([1-9][0-9]*\\)")
+              (Str.regexp "^0*\\(\\([1-9][0-9]*\\)?\\)")
               "\\1" x_no_leading_plus
           else
             (* String starts with '-' or no sign *)
             (* Strip all leading 0s, but keep the '-' sign if it's there *)
             Str.replace_first
-              (Str.regexp "^\\(-?\\)0*\\([1-9][0-9]*\\)")
+              (Str.regexp "^\\(-?\\)0*\\(\\([1-9][0-9]*\\)?\\)")
               "\\1\\2" x
         in
         (* If the input was +0*, -0* or 0*, then none of the regexps matched, so the string is now empty. Replace with "0".
            Note that if x was the empty string, then we didn't even reach the regexp matches, so we don't need to handle that case here. *)
-        if String.is_empty x_no_leading_zeros then "0" else x_no_leading_zeros
+        if String.is_empty x_no_leading_zeros || String.(=) x "-" then "0" else x_no_leading_zeros
       in
       try
         match pt with
