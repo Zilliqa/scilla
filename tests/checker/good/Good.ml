acab3daadb17f24b9d86bd38fce164107ec78c62
Georgiy Komarov <jubnzv@gmail.com>
2022-07-22 20:30:29 +0300

    [dcd] Fix false positive for unused nested library ADTs (#1105)
    Closes #1104

--- tests/checker/good/Good.ml
+++ tests/checker/good/Good.ml
@@ -90,6 +90,7 @@ module Tests = Scilla_test.Util.DiffBasedTests (struct
       "dead_code_test6.scilla";
       "dead_code_test7.scilla";
       "dead_code_test8.scilla";
+      "dead_code_test9.scilla";
       "simple-dex-remote-reads.scilla";
       "type_casts.scilla";
     ]
