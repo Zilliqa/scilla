c460ae11ba3f1463313a274aa3226d8ff39db16b
Georgiy Komarov <georgiy@zilliqa.com>
2022-12-12 21:06:57 +0700

    feat(test): Add test for duplicate field in address type

--- tests/base/parser/bad/Bad.ml
+++ tests/base/parser/bad/Bad.ml
@@ -138,6 +138,7 @@ module Tests = Scilla_test.Util.DiffBasedTests (struct
       "type_t-tid-arrow-with.scilla";
       "address_spid_as_field.scilla";
       "remote_read_namespace.scilla";
+      "duplicate_field.scilla";
     ]
 
   let exit_code : UnixLabels.process_status = WEXITED 1
