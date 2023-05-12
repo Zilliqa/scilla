d1c86f8c3f87f3bf17ebefeb70db5e9abf70878b
Yaron <yaron@zilliqa.com>
2023-05-12 13:09:03 +0300

    Change vcpkg-secp256k1 to compile & link against the secp256k1 built by vcpkg.

--- vcpkg-ocaml/vcpkg-secp256k1/src/config/discover.ml
+++ vcpkg-ocaml/vcpkg-secp256k1/src/config/discover.ml
@@ -2,22 +2,41 @@ open Base
 open Stdio
 module C = Configurator.V1
 
-let write_sexp fn sexp =
-  Out_channel.write_all fn ~data:(Sexp.to_string sexp)
-
 let () =
   C.main ~name:"secp256k1" (fun c ->
     let default : C.Pkg_config.package_conf =
-      { libs   = ["-lsecp256k1"]
+      { libs   = ["-lsecp256k1"; "-lsecp256k1_precomputed"]
       ; cflags = []
       }
     in
-    let conf =
-      match C.Pkg_config.get c with
-      | None -> default
-      | Some pc ->
-        Option.value (C.Pkg_config.query pc ~package:"libsecp256k1") ~default
+    let conf = default
     in
+    let vcpkg_cmd =
+      "python3 -c 'import os, sys; print(os.path.realpath(os.environ[\"SCILLA_REPO_ROOT\"]" ^^ "+\"/%s\"))'"
+    in
+    let vcpkg_triplet_path =
+      input_line
+        (Unix.open_process_in
+           (Printf.sprintf vcpkg_cmd "scripts/vcpkg_triplet.sh"))
+    in
+    let vcpkg_triplet =
+      input_line (Unix.open_process_in vcpkg_triplet_path)
+    in
+    let vcpkg_include_dir =
+      input_line
+        (Unix.open_process_in
+           (Printf.sprintf vcpkg_cmd
+              ("vcpkg_installed/" ^ vcpkg_triplet ^ "/include")))
+    in
+    let vcpkg_lib_dir =
+      input_line
+        (Unix.open_process_in
+           (Printf.sprintf vcpkg_cmd
+              ("vcpkg_installed/" ^ vcpkg_triplet ^ "/lib")))
+    in
+    let c_flags = List.append conf.cflags [ "-I" ^ vcpkg_include_dir ] in
+    let clib_flags = List.append conf.libs [ "-L" ^ vcpkg_lib_dir ] in
 
-    write_sexp "c_flags.sexp"         (sexp_of_list sexp_of_string conf.cflags);
-    write_sexp "c_library_flags.sexp" (sexp_of_list sexp_of_string conf.libs))
+    C.Flags.write_sexp "c_flags.sexp" c_flags;
+    C.Flags.write_sexp "c_library_flags.sexp" clib_flags)
+
