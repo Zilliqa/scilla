48d9d7f3c95b87e8655f341e9bec20755e05adb3
n-hutton <nathan.cameron.hutton@gmail.com>
2024-01-04 20:44:31 +0000

    thing!

--- handle_connection.sh
+++ handle_connection.sh
@@ -3,6 +3,7 @@
 echo "Starting server." 1>&2
 
 # Start scilla-server in the background
+rm -rf /tmp/scilla-socket/server.sock
 /scilla/0/bin/scilla-server -socket /tmp/scilla-socket/server.sock 1>&2 &
 
 # Get the PID of scilla-server
@@ -10,7 +11,9 @@ SERVER_PID=$!
 
 echo "forwarding traffic." 1>&2
 
-sleep 1
+# Block until we see the scilla server has provided the socket
+while [ ! -f /tmp/scilla-socket/server.sock ]; do sleep 1; done
+
 echo "forwarding other traffic." 1>&2
 
 # Forward traffic from stdin (provided by socat) to Unix socket.
@@ -18,6 +21,7 @@ echo "forwarding other traffic." 1>&2
 socat - UNIX-CONNECT:/tmp/scilla-socket/server.sock
 
 echo "connection closed - killing" 1>&2
+echo "" 1>&2
 
 # Kill the scilla-server process when done
 kill $SERVER_PID
